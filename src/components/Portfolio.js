29ad535daa6c1e5b25908df640555d924bf0c823
enwumeh <54593478+enwumeh@users.noreply.github.com>
2020-11-28 16:33:58 -0600

    added rails project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -150,7 +150,49 @@ const Portfolio = () => {
             </div>
           </div>
         </motion.div>
+       {/* Project 2 - Shoe Store */}
+       <motion.div
+          variants={boxVariants}
+          whileHover="hover"
+          className="projects"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://emmas-ecomm-rails.netlify.app/"
+          >
+            <div className="project-images" id="railsApp"></div>
+          </a>
 
+          <div className="project-links">
+            <div className="text">
+              <h3>Simple Rails App</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/enwumeh/Emma-s-eCommerce/blob/main/README.md"
+              >
+                <i className="fab fa-github" title="github repo" id="github">
+                  {" "}
+                </i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://emmas-ecomm-rails.netlify.app/"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
        
       </div>
       <a
