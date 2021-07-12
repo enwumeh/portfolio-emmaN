83d13b52524f06cfa4e5a3192efb75fdd45489a1
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-07-11 20:43:21 -0500

    added project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
-import React from "react"
-import "./../index.css"
-import { motion } from "framer-motion"
+import React from "react";
+import "./../index.css";
+import { motion } from "framer-motion";
 
 const boxVariants = {
   hover: {
@@ -9,7 +9,7 @@ const boxVariants = {
       type: "spring",
     },
   },
-}
+};
 
 const Portfolio = () => {
   return (
@@ -63,7 +63,47 @@ const Portfolio = () => {
             </div>
           </div>
         </motion.div>
-
+        <motion.div
+          variants={boxVariants}
+          whileHover="hover"
+          className="projects"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            // href="https://film-flix.netlify.app/"
+          >
+            <div className="project-images" id="perfumery"></div>
+          </a>
+          <div className="project-links">
+            <div className="text">
+              <h3>Essences by Emma</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                // href="https://github.com/enwumeh/filmflix"
+              >
+                <i className="fab fa-github" title="github repo" id="github">
+                  {" "}
+                </i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                // href="https://film-flix.netlify.app/ "
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
         {/* Project 2 - Shoe Store */}
         <motion.div
           variants={boxVariants}
@@ -107,7 +147,6 @@ const Portfolio = () => {
             </div>
           </div>
         </motion.div>
-
         {/* Project 3 - Quiz App */}
         <motion.div
           variants={boxVariants}
@@ -150,8 +189,8 @@ const Portfolio = () => {
             </div>
           </div>
         </motion.div>
-       {/* Project 2 - Shoe Store */}
-       {/* <motion.div
+        {/* Project 2 - Shoe Store */}
+        {/* <motion.div
           variants={boxVariants}
           whileHover="hover"
           className="projects"
@@ -193,7 +232,6 @@ const Portfolio = () => {
             </div>
           </div>
         </motion.div> */}
-       
       </div>
       <a
         rel="noopener noreferrer"
@@ -204,7 +242,7 @@ const Portfolio = () => {
         <button className="button">More Projects</button>
       </a>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
