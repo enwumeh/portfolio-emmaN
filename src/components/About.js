c3d97140f92f5b63bbd95cdf3655208df0d06285
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-08-21 11:13:12 -0500

    idk

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./../index.css";
 import photo from "../images/me-photo.jpeg";
 
-
 const About = () => {
   return (
     <section
@@ -14,24 +13,21 @@ const About = () => {
       <div className="me">
         <h1 className="heading">About me</h1>
         <p className="about-speel">
-          Hello! I’m Emma and I am a full stack developer. I love creating
-          unique user experiences from scratch! I graduated from Washington
-          University in St. Louis this past May, where I studied Cognitive Brain
-          Sciences— I was on track to be a doctor up until the beginning of my
-          final year. I had a few graduation credits left and I decided to take
-          an Intro to Computer Science course with a friend. The course was
-          thrilling and challenging...I wanted more! I decided to take a sizable
-          leap of faith and abandon my longtime journey of becoming a physician
-          to pursue programming instead. Shortly after graduation, I enrolled
-          into a coding bootcamp to dive into this newfound thing I discovered.
-          I spent 12 weeks learning full stack development, and lots of
-          frameworks and algorithms later, I am still getting my feet wet and
-          eager to learn more of the latest tech trends, and become a better
-          technologist. As a Brain Sciences major, I’m able to appreciate the
-          intersection between human behavior and technology, and exploring that
-          through building user facing interfaces is something I have been
-          excited about during this journey so far. Thanks for listening to my
-          story! I’d love to stay connected :)
+          Hello! I’m Emma and I am a full stack developer who enjoys creating
+          unique experiences from scratch. I graduated from Washington
+          University in St. Louis in 2020 where I studied Cognitive Brain
+          Science. I was on track to be a doctor up until my final year. I
+          decided to take a Computer Science course with a friend for fun. The
+          course was stimulating and challenging...which entrigued me. I decided
+          to take a sizable leap of faith and abandon my journey of
+          becoming a physician to pursue programming instead. After graduation,
+          I took a coding bootcamp to dive into this newfound thing I
+          discovered. Lots of learning has commenced, and I am still getting my
+          feet wet and eager to learn more useful technology. As a Brain
+          Sciences major, I’m able to appreciate the intersection between our
+          behavior and software, and exploring that through building interfaces
+          is something I have been excited about during this journey so far.
+          Thanks for listening to my story! I’d love to stay connected :)
         </p>
         <br></br>
         <a className="email-link-about" href="#Contact-me">
@@ -39,7 +35,7 @@ const About = () => {
         </a>
         <div id="me-div">
           <img id="me-photo" src={photo} alt="me" />
-          </div>
+        </div>
       </div>
     </section>
   );
