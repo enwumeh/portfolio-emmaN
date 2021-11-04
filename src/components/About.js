f5b132dfcde7341e2001dd390f76e900df8eecb3
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-11-03 20:00:56 -0500

    about page redited

--- src/components/About.js
+++ src/components/About.js
@@ -14,17 +14,18 @@ const About = () => {
       id="about"
     >
       <div className="me">
+      <div className="main-text">
         <h1 className="heading" id="about-heading">About me</h1>
-        <div>
+        
         <p className="about-speel">
           Hello! I’m Emma and I am a full stack developer who enjoys creating
           unique experiences from scratch. I graduated from Washington
           University in St. Louis in 2020 where I studied Cognitive Brain
           Science. I was on track to be a doctor up until my final year. I
           decided to take a Computer Science course with a friend for fun. The
-          course was stimulating and challenging...which entrigued me. <span id="read-more">I decided
+          course was stimulating and challenging...which entrigued me. I decided
           to take a sizable leap of faith and abandon my journey of
-          becoming a physician to pursue programming instead. After graduation,
+          becoming a physician to pursue programming instead.  <span id="extra-content" className="extra-content" >After graduation,
           I took a coding bootcamp to dive into this newfound thing I
           discovered. Lots of learning has commenced, and I am still getting my
           feet wet and eager to learn more useful technology. As a Brain
@@ -32,7 +33,7 @@ const About = () => {
           behavior and software, and exploring that through building interfaces
           is something I have been excited about during my journey.
             Thanks for listening to my story! I’d love to stay connected :)
-            </span>{"   "}
+            </span>
             
        <Link to="contact" className="email-link-about">
           Reach out!
